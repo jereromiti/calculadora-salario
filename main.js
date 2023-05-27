72d78733e53ed6577130b84b4bc16aaa4fac5786
jereromiti <jereromiti@gmail.com>
2023-05-27 16:05:21 -0300

    la funcion se mete dentro de un while para que vuelva a repetirse

--- main.js
+++ main.js
@@ -13,23 +13,24 @@ function descuento(x, impDecimal) {
     return (x * impDecimal);
 }
 
-function calcularSalario() {
-    // Variables Input
-    let valorHora = parseFloat(prompt("Escribe el valor hora a calcular"));
-    let horasTrabajadas = parseInt(prompt("Escribe la cantidad de horas por jornada"));
-    let diasQuincena = parseInt(prompt("Escribe la cantidad de dias a calcular"));
-    let impEntero = parseFloat(prompt("Escribe el porcentaje total de impuestos"))
-    //
-    
-    if(!isNaN(valorHora) && !isNaN(horasTrabajadas) && !isNaN(diasQuincena) && !isNaN(impEntero)){
-        let impDecimal = impuestos(impEntero)
-        let resultadoInicial = multiplica(multiplica(valorHora, horasTrabajadas), diasQuincena);
-        let descuentos = descuento(resultadoInicial, impDecimal);
-        let resultadoFinal = resta(resultadoInicial, descuentos);
-        alert(`El resultado final con ${impEntero}% de impuestos es $${resultadoFinal} pesos`)
-    }else{
-        alert("Los datos ingresados son incorrectos o estan incompletos.");
+while(true) {
+    function calcularSalario() {
+        // Variables Input
+        let valorHora = parseFloat(prompt("Escribe el valor hora a calcular"));
+        let horasTrabajadas = parseInt(prompt("Escribe la cantidad de horas por jornada"));
+        let diasQuincena = parseInt(prompt("Escribe la cantidad de dias a calcular"));
+        let impEntero = parseFloat(prompt("Escribe el porcentaje total de impuestos"))
+        //
+        if(!isNaN(valorHora) && !isNaN(horasTrabajadas) && !isNaN(diasQuincena) && !isNaN(impEntero)){
+            let impDecimal = impuestos(impEntero)
+            let resultadoInicial = multiplica(multiplica(valorHora, horasTrabajadas), diasQuincena);
+            let descuentos = descuento(resultadoInicial, impDecimal);
+            let resultadoFinal = resta(resultadoInicial, descuentos);
+            alert(`El resultado final con ${impEntero}% de impuestos es $${resultadoFinal} pesos`)
+        }else{
+            alert("Los datos ingresados son incorrectos o estan incompletos.");
+        }
     }
+    
+    calcularSalario()
 }
-
-calcularSalario()
